2a3289606eb141f24dab387895e192c3ee1a9d34
Shera3005 <aditya.sabale23@pccoepune.org>
2025-10-28 18:58:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Simple smooth scrolling for nav links
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
